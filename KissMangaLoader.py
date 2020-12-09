1fe51a816186ad786757fff2ec237d990d369abb
whosmyname <d>
2020-12-09 19:09:55 +0100

    minior prep and data

---
+++ KissMangaLoader.py
@@ -0,0 +1,15 @@
+""" some module doc-string """
+
+import os
+#import sys
+#import re
+import urllib.parse
+from argparse import ArgumentParser
+
+import requests
+
+def __main__():
+    pass
+
+if __name__ == "__main__":
+    __main__()
